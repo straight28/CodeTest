56fceac4596be2b055f3c1e22e61d06973e1e15f
straight28 <poem32@naver.com>
2019-01-26 22:36:39 +0900

    javascript/ 190126

--- javascript/Js/example.js
+++ javascript/Js/example.js
@@ -1,8 +1,8 @@
-var name = prompt("당신의 이름은?", "");
-var height = prompt("당신의 신장은??","");
-var weight = prompt("당신의 무게는??","");
+// var name = prompt("당신의 이름은?", "");
+// var height = prompt("당신의 신장은??","");
+// var weight = prompt("당신의 무게는??","");
 
-var normal_w = (height - 100) * 0.9;
-var result = weight >= normal_w - 5 && weight <= normal_w +5;
-result = result ? "적정 !!":"비적정 !!";
-document.write(name+"님은 "+result);
+// var normal_w = (height - 100) * 0.9;
+// var result = weight >= normal_w - 5 && weight <= normal_w +5;
+// result = result ? "적정 !!":"비적정 !!";
+// document.write(name+"님은 "+result);
